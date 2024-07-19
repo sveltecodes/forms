8c3cf03eaf5c41e7ec1691cf02d2b7b621ad6743
Matthew Davis <matthew@matthewdavis.io>
2024-07-18 21:46:32 -0500

    bump

--- src/lib/controls/control-container.svelte
+++ src/lib/controls/control-container.svelte
@@ -6,7 +6,7 @@
 	export let description: string = null;
 </script>
 
-<div class="flex flex-col gap-2">
+<div class="flex flex-col gap-1.5">
 	<div class="flex flex-col gap-0.5">
 		<label class={twMerge(theme.label.join(" "), "px-[1px] font-medium text-sm disabled:opacity-50", $$props.class)}>{label}</label>
 		{#if description}
